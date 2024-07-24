fb2fc7745d7fb3f79fbae388386984e67846625c
Julia K <korchkova.yulia@gmail.com>
2024-07-24 20:44:00 +0300

    feat: add loader component
    Added Loader component and style module for it. Additionally added index
    file to export components from components folder

---
+++ src/components/Loader/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import * as styles from './style.scss';
+
+export const Loader = () => {
+  return (
+    <div className={styles.loader}>
+      <div />
+      <div />
+      <div />
+      <div />
+    </div>
+  );
+};
