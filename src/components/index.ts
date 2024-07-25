461cab87b2072bafca607a6e8969aa4349e9e6d4
Julia K <korchkova.yulia@gmail.com>
2024-07-25 12:49:32 +0300

    feat: add footer component
      - The global Footer component was added, as well as the style.scss
    style file for it
      - The component was exported from folder components
      - Also the component uses the constant declared in constants routes
    FOOTER_ROUTES (RouteProps and FooterRoutesProps interfaces have been
    added for typing purposes)
      - Note: At the moment, the links in Footer don't work and don't direct
    anywhere due to lack of pages

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { Loader } from './Loader';
+export { Footer } from './Footer';
 export { Header } from './Header';
 export { Navigation } from './Navigation';
--- src/constants/routes/index.ts
+++ src/constants/routes/index.ts
@@ -7,6 +7,40 @@ export const ROUTES = {
   TIMELINE: 'timeline',
 };
 
+interface RouteProps {
+  linkLabel: string;
+  link: string;
+}
+
+interface FooterRoutesProps {
+  label: string;
+  section: Array<RouteProps>;
+}
+
+export const FOOTER_ROUTES: FooterRoutesProps[] = [
+  {
+    label: 'General',
+    section: [
+      { link: '', linkLabel: 'Market' },
+      { link: '', linkLabel: 'Service' },
+    ],
+  },
+  {
+    label: 'Product',
+    section: [
+      { link: '', linkLabel: 'Sparks' },
+      { link: '', linkLabel: 'Snaps' },
+    ],
+  },
+  {
+    label: 'Community',
+    section: [
+      { link: '', linkLabel: 'Ideas' },
+      { link: '', linkLabel: 'Streams' },
+    ],
+  },
+];
+
 export const NAV_ROUTES = [
   ['Home', ROUTES.HOME],
   ['Timeline', ROUTES.TIMELINE],
