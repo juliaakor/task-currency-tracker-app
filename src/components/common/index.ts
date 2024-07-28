e46bbf1672fd17b012425ca4ab77acb6a299f8d8
Julia K <korchkova.yulia@gmail.com>
2024-07-28 07:33:28 +0300

    feat: add input components and styles
    Input and Select component has been added, as well as their style files.
    The components are exported from components -> common folder. Also
    updated index.ts file for common components folder to include Input and
    Select components

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,4 +1,6 @@
 export { Card } from './Card';
 export { CardList } from './CardList';
+export { Input } from './Input';
 export { NavItem } from './NavItem';
+export { Select } from './Select';
 export { Switch } from './Switch';
