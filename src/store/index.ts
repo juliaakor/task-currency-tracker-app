b28418cf40e32cf1bc618419af9282d3c615e7ce
Julia K <korchkova.yulia@gmail.com>
2024-07-29 13:01:50 +0300

    feat: add redux store and currency slices
      - The following packages have been added: reduxjs/toolkit,
    react-redux, redux, redux-persist, redux-thunk
      - Added store and currencySlice
      - Added new store relative path

---
+++ src/store/index.ts
@@ -0,0 +1,37 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+import currencyReducer from './slices/currencySlice';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, currencyReducer);
+
+export const store = configureStore({
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [
+          'persist/PERSIST',
+          'persist/REHYDRATE',
+          'persist/FLUSH',
+          'persist/PAUSE',
+          'persist/REMOVE',
+          'persist/REGISTER',
+        ],
+        ignoredPaths: ['register'],
+      },
+    }),
+  reducer: {
+    currencies: persistedReducer,
+  },
+});
+
+export const persistor = persistStore(store);
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
