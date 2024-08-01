01a6d1e946e1e670d6e5c95327f287ba62ac6960
Julia K <korchkova.yulia@gmail.com>
2024-08-02 01:29:10 +0300

    refactor: update exports for global components

--- src/pages/BankCard/index.tsx
+++ src/pages/BankCard/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { BankCard } from '@components/BankCard';
+import { BankCard } from '@components/index';
 
 export const BankCardPage = () => {
   return <BankCard />;
